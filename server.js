43411e51102bec3e3ddc3a533a90c0325f5a070b
404pandas <113853550+404pandas@users.noreply.github.com>
2024-02-26 11:20:42 -0500

    Models built out

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const db = require("./config/config");
+const routes = require("./routes");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(routes);
 
 db.once("open", () => {
   app.listen(PORT, () => {
